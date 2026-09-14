b9bbe473d632559e3d1fede7cea93ed53c321dbe
emilsvifullin <181127527+emilsvifullin@users.noreply.github.com>
2026-09-14 14:15:12 +0300

    Refresh PWA cache for final swipe release fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME=
-  "sr-team-runtime-v6.22.33";
+  "sr-team-runtime-v6.22.34";
 
 const INDEX_FILE=
   "./index.html";
@@ -267,6 +267,7 @@ async function navigationResponse(
 
     const scope=
       new URL(
+        INDEX_FILE,
         self.registration.scope
       );
 

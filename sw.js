d61ad315d6fba75466a48d9aac1f282d67a102c5
emilsvifullin <181127527+emilsvifullin@users.noreply.github.com>
2026-09-14 18:39:58 +0300

    Refresh PWA cache for point card summaries

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME=
-  "sr-team-runtime-v6.22.44";
+  "sr-team-runtime-v6.22.45";
 
 const INDEX_FILE=
   "./index.html";
@@ -269,6 +269,7 @@ async function navigationResponse(
 
     const scope=
       new URL(
+        INDEX_FILE,
         self.registration.scope
       );
 

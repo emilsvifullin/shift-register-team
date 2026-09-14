78e1837d250b9c0bef8754f5baa59afa1106d32a
emilsvifullin <181127527+emilsvifullin@users.noreply.github.com>
2026-09-14 20:23:11 +0300

    Refresh PWA cache for employee card spacing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME=
-  "sr-team-runtime-v6.22.53";
+  "sr-team-runtime-v6.22.54";
 
 const INDEX_FILE=
   "./index.html";
@@ -269,6 +269,7 @@ async function navigationResponse(
 
     const scope=
       new URL(
+        INDEX_FILE,
         self.registration.scope
       );
 
